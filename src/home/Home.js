28ff10bfbe10d22a92af2512c2a5bed0fe96db41
tWhite08 <59670841+tWhite08@users.noreply.github.com>
2020-02-11 13:30:54 -0500

    Add files via upload
    Added homepage structure

---
+++ src/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import lander from "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Home</h1>
+                <p>Welcome to Fisher Bookstore.</p>
+            </div>
+        </div>
+    );
+}

f2f5550b5ddb6156b6b1a8c8d358eecdc8a1e7e5
tWhite08 <59670841+tWhite08@users.noreply.github.com>
2020-02-19 18:48:55 -0500

    Revert "Books display on page (#21)"
    This reverts commit 31c7c72b4f42f9507fd4446e1432c9575e29d1fa.

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ export default class App extends Component {
           <Navbar.Collapse className="justify-content-end">
             <Nav>
               <NavItem>
-                <Nav.Link href="/Books">Books</Nav.Link
-                <Nav.Link href="/Authors">Authors</Nav.Link>
+                <Nav.Link href="/Books">Books</Nav.Link>
               </NavItem>
             </Nav>
           </Navbar.Collapse>
--- src/books/Books.js
+++ src/books/Books.js
@@ -1,35 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Books.css";
-import { BookDisplay } from "./BookDisplay";
 
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [
-                {
-                    id: 1,
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                }
-            ]
-        };
-    }
-
-    render() {
-      return (
+export default function Books() {
+    return (
         <div className="Books">
             <div className="lander">
-                <BookDisplay books={this.state.books} />
+                <h2>Books</h2>
+                <b>Welcome to Fisher Bookstore.</b>
             </div>
         </div>
-    );  
-    }
+    );
 }

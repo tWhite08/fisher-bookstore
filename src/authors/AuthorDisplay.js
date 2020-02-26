a8cddff49f25a99bde81dfd9e07160d103f5fc75
Taylor White <taylorwhite1003@gmail.com>
2020-02-26 16:53:15 -0500

    List of Authors Achieved

---
+++ src/authors/AuthorDisplay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function AuthorDisplay(props) {
+    return (
+        <div className="display">
+            <h3>Authors</h3>
+        </div>
+    );
+}
--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Authors.css";
+import { AuthorDisplay, AuthorTableRows } from "./AuthorDisplay";
 
-export default function Authors() {
-    return (
+export default class Authors extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { //This holds states? 
+            authors: [
+                {
+                    name: "Eric Evans"
+                },
+                {
+                    name: "Nicole Forsgren"
+                },
+                {
+                    name: "Lori Jameson"
+                },
+                {
+                    name: "Jacob Delacy"
+                },
+                {
+                    name: "Red Foreman"
+                }
+            ]
+        };
+    }
+
+    AuthorTableRows = () => // Code Adapted from "Todo List" Lab 2
+      this.state.authors.map(item =>(
+        <tr key={item.name}>
+            <td>{item.name}</td>
+        </tr>
+      ));
+
+    render() {
+        return (
         <div className="Authors">
             <div className="lander">
-                <h3>Authors</h3>
-                <a>Welcome to Fisher Bookstore.</a>
+                <AuthorDisplay authors={this.state.authors} /> 
+
+                {// More code taken from "Todo List" Lab 2}
+                    }   
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            
+                        </tr>
+                     </thead>
+                    <tbody>{this.AuthorTableRows()}</tbody>
+                </table>
+
             </div>
         </div>
     );
+    }
 }

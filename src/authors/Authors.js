a6fbf204b063665ad8a82c46c36cb1679e7c3866
Taylor White <taylorwhite1003@gmail.com>
2020-03-22 14:41:12 -0400

    api function

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,58 +1,21 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./Authors.css";
-import { AuthorDisplay, AuthorTableRows } from "./AuthorDisplay";
+import { AuthorDisplay } from "./AuthorDisplay";
 
-export default class Authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { //This holds states? 
-            authors: [
-                {
-                    name: "Eric Evans"
-                },
-                {
-                    name: "Nicole Forsgren"
-                },
-                {
-                    name: "Lori Jameson"
-                },
-                {
-                    name: "Jacob Delacy"
-                },
-                {
-                    name: "Red Foreman"
-                }
-            ]
-        };
-    }
+export default function Authors(props) {
+    const [data, setData] = useState([]);
 
-    AuthorTableRows = () => // Code Adapted from "Todo List" Lab 2
-      this.state.authors.map(item =>(
-        <tr key={item.name}>
-            <td>{item.name}</td>
-        </tr>
-      ));
+    useEffect(() => {
+        fetch("https://localhost:5001/api/authors/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
 
-    render() {
-        return (
+    return (
         <div className="Authors">
             <div className="lander">
-                <AuthorDisplay authors={this.state.authors} /> 
-
-                {// More code taken from "Todo List" Lab 2}
-                    }   
-                <table className="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            
-                        </tr>
-                     </thead>
-                    <tbody>{this.AuthorTableRows()}</tbody>
-                </table>
-
+                <AuthorDisplay authors={data} />
             </div>
         </div>
     );
-    }
 }
--- src/books/Books.js
+++ src/books/Books.js
@@ -1,35 +1,21 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./Books.css";
 import { BookDisplay } from "./BookDisplay";
 
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [
-                {
-                    id: 1,
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                }
-            ]
-        };
-    }
+export default function Books(props) {
+    const [data, setData] = useState([]);
 
-    render() {
-      return (
+    useEffect(() => {
+        fetch("https://localhost:5001/api/books/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
+    return (
         <div className="Books">
             <div className="lander">
-                <BookDisplay books={this.state.books} />
+                <BookDisplay books={data} />
             </div>
         </div>
-    );  
-    }
+    );
 }
